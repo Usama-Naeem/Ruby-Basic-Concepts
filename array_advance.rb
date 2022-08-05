ec8d1b68fa615c447a41b6f306e3ad6878dc06b4
usama <usamamanghat917@gmail.com>
2022-08-05 12:32:19 +0500

    cover advanced array methods

---
+++ array_advance.rb
@@ -0,0 +1,46 @@
+#VISIT FOR DETAILED DOCS  https://ruby-doc.org/core-3.1.0/Array.html
+
+a = [18, 22, 'foo', 'cat', nil, 6, nil, 6, 22, 'foo']
+arr = [1, 2, 3, 4, 5, 6]
+
+#######################################  ACCESSING ELEMENTS ###############################
+
+# arr = [1, 2, 3, 4, 5, 6]
+# arr[2]    #=> 3
+# arr[100]  #=> nil
+# arr[-3]   #=> 4
+# arr[2, 3] #=> [3, 4, 5]
+# arr[1..4] #=> [2, 3, 4, 5]
+# arr[1..-3] #=> [2, 3, 4]
+
+# arr.fetch(100) #=> IndexError: index 100 outside of array bounds: -6...6
+# arr.fetch(100, "oops") #=> "oops"
+
+# ####################################   Destructive Selection  [Selecting Items from an Array]  ###################################3
+# select! and reject! are the corresponding destructive methods to select and reject
+# Simmilar delete_if and keep_if have the exact opposite result when supplied with the same block:
+
+########################  (1) DELETE_IF
+# arr3 = [1, 1, 2, 2, 3, 4, 3, 5, 4, 6, 5,7,9,0,9] 
+# puts arr3.delete_if {|a| arr3.count(a)>1}
+
+########################  (2) KEEP_IF
+#puts a.keep_if {|n| a.count(n)>1}
+#arr.keep_if {|a| a < 4}
+
+######################## Non-destructive Selection    [Selecting Items from an Array] #########################################
+
+######################## (1) SELECT
+
+# arr.select {|a| a > 3}       #=> [4, 5, 6]
+
+######################## (2) REJECT
+
+#puts "reject method : #{a.reject {|num| num > 10 }}\n\n"
+# arr.reject {|a| a < 3}       #=> [3, 4, 5, 6]
+
+######################## (3) DROP WHILE
+
+# arr.drop_while {|a| a < 4}   #=> [4, 5, 6]
+
+
---
+++ solve_complex_program_with_advance_arr_method.rb
@@ -0,0 +1,13 @@
+#Write a program tp\o delete all even num from array
+
+array = [2,3,7,9,6,5,10,12,14]
+
+# KEEP_IF AND DELETE_IF BOTH ARE OPPOSITE EACH OTHER
+
+#puts array.delete_if {|n| n.modulo(2)== 0 }
+#puts array.keep_if {|n| n.modulo(2)== 0 }
+
+# SELECT AND REJECT BOTH ARE OPPOSITE EACH OTHER
+
+#puts array.select {|n| n.modulo(2)==0}
+#puts array.reject {|n| n.modulo(2)==0}
